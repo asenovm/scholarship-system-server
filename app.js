3a9c0f689e30ae4311c4bd3da18df8c8edccb70c
toshle <rosen_ra4ev@abv.bg>
2013-10-26 17:35:33 +0300

    Move login to separate module
    Signed-off-by: toshle <rosen_ra4ev@abv.bg>

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var expressValidator = require('express-validator'); 
 var registration = require('./services/register'); 
+var login = require('./services/login'); 
 
 
 var databaseURL = '192.168.0.103:27017/scholar-systems';
@@ -38,35 +39,8 @@ app.configure(function() {
 }));
 });
 
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-	function(username, password, done) {
-    db.users.findOne({email: username, password: password}, function(err, user) {
-      if(err) {
-        return done(err);
-      } 
-      if(!user) {
-        return done(null, false, { message: 'Invalid username or password.'});
-      }
-      console.log(user);
-      return done(null, user);
-    });
-  }
-));
-
-app.post('/login', passport.authenticate('local'), function(req, res) {
-  res.send(req.user);
-});
-
+login.route(app, db, passport, LocalStrategy);
 registration.route(app,db);
 
 
---
+++ services/login.js
@@ -0,0 +1,30 @@
+exports.route = function(app, db, passport, LocalStrategy) {
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use(new LocalStrategy(
+	  function(username, password, done) {
+      db.users.findOne({email: username, password: password}, function(err, user) {
+        if(err) {
+          return done(err);
+        } 
+        if(!user) {
+          return done(null, false, { message: 'Invalid username or password.'});
+        }
+        console.log(user);
+        return done(null, user);
+      });
+    }
+  ));
+
+  app.post('/login', passport.authenticate('local'), function(req, res) {
+    res.send(req.user);
+  });
+};
