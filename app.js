22dbb1522f796a113597e122da420ac22eecaa9f
toshle <rosen_ra4ev@abv.bg>
2013-10-26 14:11:53 +0300

    Add login with passport
    Signed-off-by: toshle <rosen_ra4ev@abv.bg>

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 var express = require('express');
 var mongojs = require('mongojs');
+var passport = require('passport');
+var crypto = require('crypto-js/md5');
+var LocalStrategy = require('passport-local').Strategy;
 
-var databaseURL = 'mydb';
+
+var databaseURL = '192.168.0.103:27017/scholar-systems';
 var collections = ['users'];
 var db = mongojs.connect(databaseURL, collections);
 
 var app = express();
-app.use(express.bodyParser());
 
-app.post('/login', function(req, res) {
-  var username = req.param('username');
-  var password = req.param('password');
-  db.users.findOne({ username: username, password: password }, function(err, user) {
-    if(err || !user) res.send('Invalid login');
-    else res.send(user);
+app.configure(function() {
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
+});
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+    db.users.findOne({email: username, password: password}, function(err, user) {
+      if(err) {
+        return done(err);
+      } 
+      if(!user) {
+        return done(null, false, { message: 'Invalid username or password.'});
+      }
+      console.log(user);
+      return done(null, user);
+    });
+  }
+));
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true}), function(req, res) {
+  
+  res.send(req.user);
+  
 });
 
 app.get('/register', function(req, res) {
